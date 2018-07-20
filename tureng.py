9e9b02815853e3bf2e40659c97823f7bf6153105
veli <realveli24@gmail.com>
2018-07-20 16:05:56 +0300

    Write-limit Feature Added Bugs Fixed. Junk Error Handling Features Deleted. New Error Handlings Setted Start By Module Feature Added.

--- tureng.py
+++ tureng.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
-import requests
-from bs4 import BeautifulSoup
 import re
-import frames
 import sys
 
+import requests
+from bs4 import BeautifulSoup
+import frames
 
 class Translater:
 
     def __init__(self):
         self.main_url = "http://tureng.com/tr/turkce-ingilizce/{}"
         self.subject_dict = {}
-        self.counter = 1
         self.mytds = ""
 
     def word_crawler(self, word, type):
@@ -41,13 +40,18 @@ class Translater:
         word = word.decode("utf-8")
         word = word.replace("['", "")
         word = word.replace("']", "")
-        webpage = requests.get(self.main_url.format(word))
+        try:
+            webpage = requests.get(self.main_url.format(word))
+        except requests.exceptions.ConnectionError:
+            print("Network Error, Please Check our Connection")
+            sys.exit()
         content = webpage.content
         soup = BeautifulSoup(content, "lxml")
         self.mytds = soup.findAll("td", {"class": "hidden-xs"})
 
     def subject_remaker(self, word_t):
         pool = ""
+        counter = 1
         self.subject_crawler(word_t)
         for i in self.mytds:
             i = str(i)
@@ -71,43 +75,47 @@ class Translater:
         pool = pool.split(",")
         for i in pool:
             if i != "":
-                self.subject_dict.update({self.counter: i})
-                self.counter += 1
-        self.counter = 1
+                self.subject_dict.update({counter: i})
+                counter += 1
 
     def writer(self, main_word, type):
+        frame1 = None
+        write_time = 20
         counter2 = 1
         if type == "en tm":
             frame1 = frames.frame1_tr
-        if type == "tr ts":
+        elif type == "tr ts":
             frame1 = frames.frame1_en
-        print(frame1)
+        if counter2 > write_time:
+            sys.exit()
         self.subject_remaker(main_word)
+        print(frame1)
         for i in self.word_remaker(main_word, type).split("\n"):
-            try:
-                counter_sub = len(self.subject_dict.get(counter2))
-            except:
-                break
-            i = str(i).replace("</a> <i>", "")
-            i = i.replace(r" </i>\n</td>']", "")
-            i = i.replace(r' </i>\n</td>"]', "")
-            i = i.replace('["', "")
-            i = i.replace("i.", "")
-            main_word = str(main_word).replace("['", "")
-            main_word = main_word.replace("']", "")
-            a = frames.frame2.format(
-                str(counter2)+" "*(4 - len(str(counter2))),
-                self.subject_dict.get(counter2)+(16-counter_sub)*" ",
-                main_word,
-                i,)
-            counter_a = len(a)
-            a += ((82 - counter_a)*" " + "|")
-            print(a)
-            counter2 += 1
+            counter_sub = self.subject_dict.get(counter2)
+            if counter_sub is not None:
+                counter_sub = len(counter_sub)
+                i = str(i).replace("</a> <i>", "")
+                i = i.replace(r" </i>\n</td>']", "")
+                i = i.replace(r' </i>\n</td>"]', "")
+                i = i.replace('["', "")
+                i = i.replace("i.", "")
+                main_word = str(main_word).replace("['", "")
+                main_word = main_word.replace("']", "")
+                a = frames.frame2.format(
+                    str(counter2) + " " * (4 - len(str(counter2))),
+                    self.subject_dict.get(counter2) + (16 - counter_sub) * " ",
+                    main_word,
+                    i, )
+                counter_a = len(a)
+                a += ((82 - counter_a) * " " + "|")
+                print(a)
+                counter2 += 1
+        else:
+            pass
         print(frames.frame_last)
 
 
-if __name__ == "__main__":
+if __name__ == "__main__" or "tureng":
     main = Translater()
     if sys.argv[1:2] == ['en']:
         main.writer(sys.argv[2:], "en tm")
